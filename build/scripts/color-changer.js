061977af2f739d774fd57246d975ad1738d51184
Nick Menghini <nick.menghini@gmail.com>
2022-07-23 14:22:08 -0400

    Adding pantone year picker
    adding some fun color changing nonsense

---
+++ build/scripts/color-changer.js
@@ -0,0 +1,41 @@
+$('#2017').on('click', function() {
+    $('html, body').css('background', '#88B04B');
+    $('h1,h2,h3,h4,h5,h6,p,a,text-large').css('color', '#f4f4f9');
+    $('.svg-background').css('fill', '#f4f4f9');
+    $('.svg-mark').css('fill', '#88B04B');
+});
+
+$('#2018').on('click', function() {
+    $('html, body').css('background', '#604c8d');
+    $('h1,h2,h3,h4,h5,h6,p,a,text-large').css('color', '#f4f4f9');
+    $('.svg-background').css('fill', '#f4f4f9');
+    $('.svg-mark').css('fill', '#604c8d');
+});
+
+$('#2019').on('click', function() {
+    $('html, body').css('background', '#FA7268');
+    $('h1,h2,h3,h4,h5,h6,p,a,text-large').css('color', '#f4f4f9');
+    $('.svg-background').css('fill', '#f4f4f9');
+    $('.svg-mark').css('fill', '#FA7268');
+}); 
+
+$('#2020').on('click', function() {
+    $('html, body').css('background', '#34558b');
+    $('h1,h2,h3,h4,h5,h6,p,a,text-large').css('color', '#f4f4f9');
+    $('.svg-background').css('fill', '#f4f4f9');
+    $('.svg-mark').css('fill', '#34558b');
+}); 
+
+$('#2021').on('click', function() {
+    $('html, body').css('background', '#939597');
+    $('h1,h2,h3,h4,h5,h6,p,a,text-large').css('color', '#F5DF4D');
+    $('.svg-background').css('fill', '#F5DF4D');
+    $('.svg-mark').css('fill', '#939597');
+});
+
+$('#2022').on('click', function() {
+    $('html, body').css('background', '#6667AB');
+    $('h1,h2,h3,h4,h5,h6,p,a,text-large').css('color', '#f4f4f9');
+    $('.svg-background').css('fill', '#f4f4f9');
+    $('.svg-mark').css('fill', '#6667AB');
+});
