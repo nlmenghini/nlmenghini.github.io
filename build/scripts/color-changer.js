221ecf22849b53a849087effc67f3393e9acd5d3
Nick Menghini <nick.menghini@gmail.com>
2023-08-06 16:40:33 -0400

    updating logo
    updating logo

--- build/scripts/color-changer.js
+++ build/scripts/color-changer.js
@@ -1,48 +1,41 @@
 $('#2017').on('click', function() {
     $('html, body').css('background', '#88B04B');
     $('h1,h2,h3,h4,h5,h6,p,a,text-large').css('color', '#f4f4f9');
-    $('.svg-background').css('fill', '#f4f4f9');
-    $('.svg-mark').css('fill', '#88B04B');
+    $('.svg-mark').css('fill', '#f4f4f9');
 });
 
 $('#2018').on('click', function() {
     $('html, body').css('background', '#604c8d');
     $('h1,h2,h3,h4,h5,h6,p,a,text-large').css('color', '#f4f4f9');
-    $('.svg-background').css('fill', '#f4f4f9');
-    $('.svg-mark').css('fill', '#604c8d');
+    $('.svg-mark').css('fill', '#f4f4f9');
 });
 
 $('#2019').on('click', function() {
     $('html, body').css('background', '#FA7268');
     $('h1,h2,h3,h4,h5,h6,p,a,text-large').css('color', '#f4f4f9');
-    $('.svg-background').css('fill', '#f4f4f9');
-    $('.svg-mark').css('fill', '#FA7268');
+    $('.svg-mark').css('fill', '#f4f4f9');
 }); 
 
 $('#2020').on('click', function() {
     $('html, body').css('background', '#34558b');
     $('h1,h2,h3,h4,h5,h6,p,a,text-large').css('color', '#f4f4f9');
-    $('.svg-background').css('fill', '#f4f4f9');
-    $('.svg-mark').css('fill', '#34558b');
+    $('.svg-mark').css('fill', '#f4f4f9');
 }); 
 
 $('#2021').on('click', function() {
     $('html, body').css('background', '#939597');
     $('h1,h2,h3,h4,h5,h6,p,a,text-large').css('color', '#F5DF4D');
-    $('.svg-background').css('fill', '#F5DF4D');
-    $('.svg-mark').css('fill', '#939597');
+    $('.svg-mark').css('fill', '#F5DF4D');
 });
 
 $('#2022').on('click', function() {
     $('html, body').css('background', '#6667AB');
     $('h1,h2,h3,h4,h5,h6,p,a,text-large').css('color', '#f4f4f9');
-    $('.svg-background').css('fill', '#f4f4f9');
-    $('.svg-mark').css('fill', '#6667AB');
+    $('.svg-mark').css('fill', '#f4f4f9');
 });
 
 $('#2023').on('click', function() {
     $('html, body').css('background', '#BB2649');
     $('h1,h2,h3,h4,h5,h6,p,a,text-large').css('color', '#f4f4f9');
-    $('.svg-background').css('fill', '#f4f4f9');
-    $('.svg-mark').css('fill', '#BB2649');
+    $('.svg-mark').css('fill', '#f4f4f9');
 });
